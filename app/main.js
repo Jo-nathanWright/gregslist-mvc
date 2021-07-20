959693635819bbf508a8c08a086b85c0183a8d38
Jonathan Wright <j.wright72127@gmail.com>
2021-07-20 15:44:38 -0600

    Got rid of the Game Form

--- app/main.js
+++ app/main.js
@@ -1,13 +1,11 @@
 import CarsController from "./Controllers/CarsController.js";
 import HousesController from "./Controllers/HousesController.js"
 import JobsContoller from "./Controllers/JobsController.js"
-import GamesController from "./Controllers/GamesController.js"
 
 class App {
   carsController = new CarsController()
   housesController = new HousesController()
   jobsController = new JobsContoller()
-  gamesController = new GamesController()
 }
 
 window["app"] = new App();
